0683683463e6b5e64418bfd6bee5d1288ee14dcf
fi-ya <safiaali@hotmail.com>
2022-03-30 12:28:26 +0100

    refactor: make grid global variable, remove used code/comments

--- app.rb
+++ app.rb
@@ -8,37 +8,28 @@ require './lib/game'
 require './lib/app_builder'
 
 set :default_content_type, 'application/json'
-
 set :allow_origin, "http://localhost:3000"
 set :allow_methods, "GET,HEAD,POST,PUT,PATCH"
 set :allow_headers, "content-type, if-modified-since, access-control-allow-origin" 
 set :expose_headers, "location,link"
 
-#  global variables
-$app_builder = AppBuilder.new(%w[1 2 3 4 5 6 7 8 9])
-p "APP BUILDER:", $app_builder
-
-before do
-  # $app_builder =  AppBuilder.new(%w[1 2 3 4 5 6 7 8 9])
-  # request.body.rewind
-  # @request_payload = JSON.parse request.body.read
-end
+$grid = %w[1 2 3 4 5 6 7 8 9]
+$app_builder = AppBuilder.new($grid)
 
 # Endpoints
 get "/" do
-  data = { 'grid' => [1,2,3,4,5,6,7,8,9] }
+  data = { 'grid' => $grid }
   data.to_json
 end
 
 get "/start-game" do
   $app_builder = AppBuilder.new(%w[1 2 3 4 5 6 7 8 9])
-  $app_builder.board.reset_grid
   response = { 'current_player' => $app_builder.game.current_player.marker }
   response.to_json
 end
 
 get "/start-game/grid" do
-  response = { 'current_player' => @game.current_player.marker, 'grid' => @game.board.grid}
+  response = { 'current_player' => $app_builder.game.current_player.marker, 'grid' => $app_builder.game.board.grid}
   response.to_json
 end
 
@@ -49,32 +40,13 @@ put "/start-game/grid" do
 
   game_status = $app_builder.game.take_turn(@request_payload[0], @request_payload[1].to_i)
 
-  response = { 'current_player' => $app_builder.game.current_player.marker, 'grid' => $app_builder.game.board.grid,
-     'game_status'=> "#{game_status}"
+  response = { 
+    'current_player' => $app_builder.game.current_player.marker, 'grid' => $app_builder.game.board.grid,
+    'game_status'=> "#{game_status}"
     }
   p 'Grid Move response', response
   response.to_json
 end
-
-# post "/grid/:move" do
- 
-#   p "REQUEST BODY REWIND: ", request.body
-#   @request_payload = JSON.parse request.body.read
-#   p "REQUEST PAYLOAD: ",@request_payload.to_i
-
-#   @game.play_turn(@game.current_player.marker, @request_payload.to_i)
-  
-#   response = { 'current_player' => @game.current_player.marker, 'grid' => @game.board.grid}
-#   response.to_json
-# end
-
-# get "/grid" do
-#   board = Board.new
-#   grid = board.reset_grid
-  
-#   data = { 'grid' => grid }
-#   data.to_json
-# end
 
 # get '/game-mode' do
 #   data = JSON.parse(request.body).string
