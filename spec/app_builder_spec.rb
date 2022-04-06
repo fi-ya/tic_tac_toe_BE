c924839018546f4e14170f05a02e37d4f22efbcb
fi-ya <safiaali@hotmail.com>
2022-04-06 12:49:13 +0100

    wip : add app_builder_spec file

---
+++ spec/app_builder_spec.rb
@@ -0,0 +1,10 @@
+require 'app_builder'
+
+RSpec.describe AppBuilder do 
+
+  describe "#initialize" do 
+    xit 'should create instances of Board, GameMode, HumanPlayer & Game' do
+
+    end
+  end
+end
