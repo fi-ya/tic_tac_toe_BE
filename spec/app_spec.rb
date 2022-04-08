177f9d0238e3c1cb6aea94d1011e4f397003f017
fi-ya <safiaali@hotmail.com>
2022-04-08 12:11:28 +0100

    test: '/start-game/:player1_token' returns 200 OK for both 1 & 2 routes

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -8,9 +8,38 @@ RSpec.describe 'app' do
     Sinatra::Application
   end
 
-  it "says hello" do
-    get '/'
-    expect(last_response).to be_ok
-    expect(last_response.body).to eq('Hello World')
+  context "GET to /" do
+    let(:response) { get '/' }
+    it 'reponse returns hello world' do
+      expect(response.body).to eq('Hello World')
+    end
+
+    it 'returns status 200 OK' do
+      expect(response.status).to eq(200)
+      expect(response).to be_ok
+    end
+  end
+  describe "GET to /start-game/:player1_token" do
+
+    context "GET human vs human game" do 
+      let(:response){ get '/start-game/1'}
+
+      it 'returns status 200 OK' do
+        expect(response.status).to eq(200)
+        expect(response).to be_ok
+      end
+
+    end
+
+    context "GET computer vs human game" do 
+      let(:response){ get '/start-game/2'}
+
+      it 'returns status 200 OK' do
+        expect(response.status).to eq(200)
+        expect(response).to be_ok
+      end
+
+    end
+
   end
 end
