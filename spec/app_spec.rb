e08b5c68b6a0d32b6e210389272c179a7bd763ed
fi-ya <safiaali@hotmail.com>
2022-04-08 13:36:15 +0100

    test: response.by of '/start-game/' routes 1 & 2

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -23,10 +23,20 @@ RSpec.describe 'app' do
 
     context "GET human vs human game" do 
       let(:response){ get '/start-game/1'}
+      
 
       it 'returns status 200 OK' do
-        expect(response.status).to eq(200)
+        p 'request', response
+        expect(response.status).to eq 200
         expect(response).to be_ok
+      end
+
+      it 'headers include content-type' do 
+        expect(response.headers["Content-Type"]).to eq("application/json")
+      end
+      
+      it 'returns response body including reset_current_player, reset_current_marker & new_grid' do 
+        expect(response.body).to include("{\"reset_current_player1_name\":\"Human\",\"reset_current_player_marker\":\"X\",\"new_grid\":\"[\\\"1\\\", \\\"2\\\", \\\"3\\\", \\\"4\\\", \\\"5\\\", \\\"6\\\", \\\"7\\\", \\\"8\\\", \\\"9\\\"]\"}")
       end
 
     end
@@ -35,10 +45,17 @@ RSpec.describe 'app' do
       let(:response){ get '/start-game/2'}
 
       it 'returns status 200 OK' do
-        expect(response.status).to eq(200)
+        expect(response.status).to eq 200
         expect(response).to be_ok
       end
 
+      it 'headers include content-type' do 
+        expect(response.headers["Content-Type"]).to eq("application/json")
+      end
+      
+      it 'returns response body including reset_current_player, reset_current_marker & new_grid' do 
+        expect(response.body).to include("{\"reset_current_player1_name\":\"Computer\",\"reset_current_player_marker\":\"X\",\"new_grid\":\"[\\\"1\\\", \\\"2\\\", \\\"3\\\", \\\"4\\\", \\\"5\\\", \\\"6\\\", \\\"7\\\", \\\"8\\\", \\\"9\\\"]\"}")
+      end
     end
 
   end
